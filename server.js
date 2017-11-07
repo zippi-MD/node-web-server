0eda03f08b1a26cc555940b496421dff0740fcac
Alejandro Mendoza <zippi180@gmail.com>
2017-11-06 21:28:15 -0600

    Added projects path

--- server.js
+++ server.js
@@ -56,6 +56,13 @@ app.get('/bad', (request, response) => {
     });
 });
 
+app.get('/projects', (request, response) => {
+   response.render('projects.hbs',{
+     pageTitle: 'Projects Page',
+     message: 'Portfolio Page'
+   });
+});
+
 app.listen(port, () => {
     console.log(`Server is up on port: ${ port }`);
 });
